45c62bc0a1ab920dfd157b18775790793f0c7719
Evan de Jesus <evan.dejesus@24g.com>
2020-02-17 21:52:28 -0500

    update backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.port || 8080;
 
-app.get((req, res) => {
+app.get('/', (req, res) => {
     res.send('OK');
+})
+
+app.post('/passenger', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 })
 
 app.listen(port, () => {
