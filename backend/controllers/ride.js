6a6e9e5ad1a0b8f7950e8a4ccb64187065e16e2f
Evan de Jesus <evan.dejesus@24g.com>
2020-02-24 16:46:28 -0500

    remove passenger adding from ride.post

--- backend/controllers/ride.js
+++ backend/controllers/ride.js
@@ -16,37 +16,43 @@ module.exports.get = (req, res) => {
 module.exports.post = (req, res) => {
   let sql;
   let rideId;
+  const driverId = req.body.driver_access_id;
   db.query('START TRANSACTION')
     .then(() => {
       sql = 'SELECT driver.id FROM driver INNER JOIN passenger where passenger.access_id=?';
-      return db.query(sql, [req.body.driver_access_id]);
+      return db.query(sql, [driverId]);
     })
     .then(rows => {
+      if (rows.length === 0) {
+        const errString = `driver with access id ${driverId} not found`;
+        respond(400, errString, res);
+        throw errString;
+      }
       sql = 'INSERT INTO ride (driver_id, departure_time, location) VALUES (?, ?, ?)';
       return db.query(sql, [rows[0].id, req.body.departure_time, req.body.location]);
     })
-    .then(rows => {
-      rideId = rows.insertId;
-      if (req.body.passengers) {
-        sql = 'SELECT id FROM passenger WHERE access_id in ?';
-        const values = [
-          req.body.passengers
-        ];
-        return db.query(sql, [values])
-      }
-    })
-    .then(rows => {
-      let values = [];
-      rows.forEach(row => {
-        values.push([
-          rideId,
-          row.id
-        ]);
-      });
+    // .then(rows => {
+    //   rideId = rows.insertId;
+    //   if (req.body.passengers) {
+    //     sql = 'SELECT id FROM passenger WHERE access_id in ?';
+    //     const values = [
+    //       req.body.passengers
+    //     ];
+    //     return db.query(sql, [values])
+    //   }
+    // })
+    // .then(rows => {
+    //   let values = [];
+    //   rows.forEach(row => {
+    //     values.push([
+    //       rideId,
+    //       row.id
+    //     ]);
+    //   });
 
-      sql = 'INSERT INTO ride_passenger_join (ride_id, passenger_id) VALUES ?';
-      return db.query(sql, [values]);
-    })
+    //   sql = 'INSERT INTO ride_passenger_join (ride_id, passenger_id) VALUES ?';
+    //   return db.query(sql, [values]);
+    // })
     .then(rows => {
       db.query('COMMIT');
       respond(200, rows, res);
@@ -59,3 +65,5 @@ module.exports.post = (req, res) => {
         });
     });
 }
+
+module.exports.put = (req, res) => { };
