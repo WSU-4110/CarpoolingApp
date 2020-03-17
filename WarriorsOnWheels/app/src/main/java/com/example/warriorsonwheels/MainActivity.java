061278106c9b5b73282354777a2c58d6f1c9daa7
Darpan Shah <gg2002@wayne.edu>
2020-03-17 19:19:51 -0400

    Profile shown in new intent, google account is a global var

--- WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/MainActivity.java
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.warriorsonwheels;
 
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
