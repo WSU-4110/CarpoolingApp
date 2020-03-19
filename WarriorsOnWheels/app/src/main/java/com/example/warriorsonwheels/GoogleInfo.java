ea28ab3c2103a7463fed8667d3df65766f17028f
Darpan Shah <gg2002@wayne.edu>
2020-03-19 15:01:59 -0400

    Added initial API push

--- WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/GoogleInfo.java
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/GoogleInfo.java
@@ -7,11 +7,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -23,9 +30,14 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 public class GoogleInfo extends AppCompatActivity implements View.OnClickListener  {
+
+    private RequestQueue queue;
 
     private LinearLayout profileSection;
     private Button signOut;
@@ -40,6 +52,8 @@ public class GoogleInfo extends AppCompatActivity implements View.OnClickListene
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.google_info);
+
+        queue = Volley.newRequestQueue(this);
 
         Name = (TextView)findViewById(R.id.nameDisplay);
         Email = (TextView)findViewById(R.id.emailDisplay);
@@ -82,6 +96,60 @@ public class GoogleInfo extends AppCompatActivity implements View.OnClickListene
 
         Intent signOut = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(signOut);
+    }
+
+    private StringRequest searchNameStringRequest(String nameSearch) {
+        final String API = "&api_key=<<YOUR_API_KEY_HERE>>";
+//        final String NAME_SEARCH = "&q=";
+//        final String DATA_SOURCE = "&ds=Standard Reference";
+//        final String FOOD_GROUP = "&fg=";
+//        final String SORT = "&sort=r";
+//        final String MAX_ROWS = "&max=25";
+//        final String BEGINNING_ROW = "&offset=0";
+        final String URL_PREFIX = "https://carpool-api-r64g2xh4xa-uc.a.run.app/rating/ab1234";
+
+ //       String url = URL_PREFIX + API + NAME_SEARCH + nameSearch + DATA_SOURCE + FOOD_GROUP + SORT + MAX_ROWS + BEGINNING_ROW;
+        String url = URL_PREFIX;
+
+        // 1st param => type of method (GET/PUT/POST/PATCH/etc)
+        // 2nd param => complete url of the API
+        // 3rd param => Response.Listener -> Success procedure
+        // 4th param => Response.ErrorListener -> Error procedure
+        return new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    // 3rd param - method onResponse lays the code procedure of success return
+                    // SUCCESS
+                    @Override
+                    public void onResponse(String response) {
+                        // try/catch block for returned JSON data
+                        // see API's documentation for returned format
+                        try {
+                            JSONObject result = new JSONObject(response).getJSONObject("");
+                            int maxItems = result.getInt("user_id");
+                            JSONArray resultList = result.getJSONArray("");
+
+                            Name.setText(result.toString());
+
+
+
+
+
+
+                            // catch for the JSON parsing error
+                        } catch (JSONException e) {
+                            Toast.makeText(GoogleInfo.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    } // public void onResponse(String response)
+                }, // Response.Listener<String>()
+                new Response.ErrorListener() {
+                    // 4th param - method onErrorResponse lays the code procedure of error return
+                    // ERROR
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // display a simple message on the screen
+                        Toast.makeText(GoogleInfo.this, "Food source is not responding (USDA API)", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
 
