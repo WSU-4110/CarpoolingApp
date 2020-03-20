4566d29edca3bcc57af69bb3d3336fd5fbfae9a9
Saloni Gupta <gh4683@wayne.edu>
2020-03-19 22:56:53 -0400

    Added CreateRide java and xml files

---
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/CreateRide.java
@@ -0,0 +1,21 @@
+package com.example.warriorsonwheels;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateRide extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createridelayout);
+
+
+    }
+}
