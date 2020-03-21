9b0a147840a4e13c691cd4930f51c8c1d0033696
nidhi-nadig10 <gh6210@wayne.edu>
2020-03-20 22:45:48 -0400

    connect to postride.java

--- WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/HomePage.java
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/HomePage.java
@@ -7,14 +7,38 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePage extends AppCompatActivity{
+
+    private Button findRideButton;
+    private Button postRideButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createridelayout);
 
+        //Button Ids
+        findRideButton = (Button)findViewById(R.id.findRideButton);
+        postRideButton = (Button)findViewById(R.id.postRideButton);
+
+    }
+
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            //Go to RideSearch.java
+            case R.id.findRideButton:
+                Intent intent1 = new Intent(getApplicationContext(), RideSearch.class);
+                startActivity(intent1);
+
+                //Go to PostRide.java
+            case R.id.postRideButton:
+                Intent intent2 = new Intent(getApplicationContext(), PostRide.class);
+                startActivity(intent2);
+        }
     }
 }
