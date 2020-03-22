bef5de9f51c4ccc4e6a9f7e18a81f185f6292525
nidhi-nadig10 <gh6210@wayne.edu>
2020-03-22 16:01:56 -0400

    updated userprofile

--- WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/UserProfile.java
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/UserProfile.java
@@ -1,4 +1,32 @@
 package com.example.warriorsonwheels;
 
-public class UserProfile {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfile extends AppCompatActivity {
+
+    //Pass Prof Var
+    private TextView name, accessID, phNum, primLoc, passRating;
+
+    //Driver Prof Var
+    private TextView carMake, carModel, carYear, carColor, licensePlate, drivRating;
+
+    private Toolbar tbrMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userprofile);
+
+        //Toolbar
+        tbrMain = findViewById(R.id.tbrMain);
+        setSupportActionBar(tbrMain);
+
+        
+    }
+
 }
