e06bcea5289cf3fb72e5990f845097b9c65a8809
nidhi-nadig10 <gh6210@wayne.edu>
2020-03-23 17:20:38 -0400

    update findpassengers.java

--- WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/FindPassengers.java
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/FindPassengers.java
@@ -1,6 +1,16 @@
 package com.example.warriorsonwheels;
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class FindPassengers {
+public class FindPassengers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.findpassengers);
+
+    }
 }
