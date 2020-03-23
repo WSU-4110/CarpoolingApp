95245ded78c6a6afb50bccef3f411374764a4230
nidhi-nadig10 <gh6210@wayne.edu>
2020-03-22 22:32:26 -0400

    overflow menu added to all pages

--- WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/PostRide.java
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/PostRide.java
@@ -2,6 +2,9 @@ package com.example.warriorsonwheels;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,6 +45,38 @@ public class PostRide extends AppCompatActivity {
         //Set Time Picker to 24 hours
         leaveTimePicker.setIs24HourView(true);
 
+    }
+
+    //Create Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.overflowmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //Menu Options
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.homePage:
+                Intent intent1 = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intent1);
+                return true;
+
+            case R.id.userProfilePage:
+                Intent intent2 = new Intent(getApplicationContext(), UserProfile.class);
+                startActivity(intent2);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     public void onClick (View v) {
--- WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/UserProfile.java
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/UserProfile.java
@@ -1,6 +1,10 @@
 package com.example.warriorsonwheels;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
@@ -40,6 +44,38 @@ public class UserProfile extends AppCompatActivity {
         licensePlate = findViewById(R.id.licensePlate);
         drivRating = findViewById(R.id.drivRating);
 
+    }
+
+    //Create Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.overflowmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //Menu Options
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.homePage:
+                Intent intent1 = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intent1);
+                return true;
+
+            case R.id.userProfilePage:
+                Intent intent2 = new Intent(getApplicationContext(), UserProfile.class);
+                startActivity(intent2);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 
