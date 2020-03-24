13059a5cb7f61ec4dac1286c590cdaae38d3ba1d
Darpan Shah <gg2002@wayne.edu>
2020-03-23 21:00:13 -0400

    updated passenger count

--- WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/FindPassengers.java
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/FindPassengers.java
@@ -1,23 +1,4 @@
 package com.example.warriorsonwheels;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-public class FindPassengers extends AppCompatActivity {
-
-    //Vars
-    private Toolbar tbrMain;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.findpassengers);
-
-        //Toolbar
-        tbrMain =  findViewById(R.id.tbrMain);
-        setSupportActionBar(tbrMain);
-
-    }
+public class FindPassengers {
 }
