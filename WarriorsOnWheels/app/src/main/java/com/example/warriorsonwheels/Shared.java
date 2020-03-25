c3a4da7184cf8d7596656ebb67ae775fd31b3807
nidhi-nadig10 <gh6210@wayne.edu>
2020-03-25 15:48:33 -0400

    SINGLETON DESIGN PATTERN IMPLEMENTATION IN SHARED.JAVA

--- WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/Shared.java
+++ WarriorsOnWheels/app/src/main/java/com/example/warriorsonwheels/Shared.java
@@ -1,36 +1,52 @@
+/*
+ * //////////////////////////////////////////////////////////////////////
+ *
+ *           Assignment 5: DESIGN PATTERNS
+ *       Nidhi Nadig     gh6210      3/25/2020
+ *   Functional Requirement: Shared class sends Google Information to be displayed
+ *       in the app
+ *
+ *   Below is the Shared.java file from our android application, modified
+ *   to fit the Singleton design pattern.
+ *
+ * //////////////////////////////////////////////////////////////////////
+ */
+
 package com.example.warriorsonwheels;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public enum Shared {
+
     //initiate enum shared variables
     Data;
     public int selectedRide = 0;
     public boolean isDriver = false;
-    public int profPic;
+    public int profPic = 0;
     public String userName, userId, userLoc, phNumber,driverAccessID;
-
     GoogleSignInAccount googleAccount;
-
     String name, email, imgURL;
-
 
     public void setGoogleAccount(GoogleSignInAccount account)
     {
-        googleAccount = account;
+        private  googleAccount instance = null;
 
-        name = account.getDisplayName();
-        email = account.getEmail();
-        imgURL = account.getPhotoUrl().toString();
+        private setGoogleAccount() {
 
-
-
+        accountInfo = googleAccount(name, email, imgURL)
+    }
     }
 
     public GoogleSignInAccount getGoogleAccount()
     {
+        if (instance = null) {
+
+            instance = new setGoogleAccount();
+        }
+
         return googleAccount;
     }
-
 }
+
+    Shared accountInfo = Shared.getGoogleAccount();
 
