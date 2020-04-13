5ec7e3b430e4e996f1af1f080cb88a9e96f98a4f
Evan de Jesus <evan.dejesus@24g.com>
2020-04-13 17:00:35 -0400

    begin firebase implementation

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const router = require('./routes/index');
 const models = require('./models');
 
-const firebase = require('./controllers/firebase');
+// const firebase = require('./controllers/firebase');
 
 const app = express();
 const port = process.env.port || 8080;
--- backend/controllers/firebase.js
+++ backend/controllers/firebase.js
@@ -1,17 +1,26 @@
 const admin = require('firebase-admin');
 
 admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: 'https://carpooling-app-271518.firebaseio.com'
-  });
-  
-  var registrationToken = 'cuY66e1BRSewMBDpncXfmz:APA91bGY3o9K7IAOWtiElHysVHj690j9QlOwenSjswfC_FsaE886dYNAu7TvxkKLdT59WNJ6_xrMzYeLdObX-D5z_s11JXg7PH4IRu3ocfurQYVSJ2PzKUvhZYpcZpraGY3-HtXzfjeh';
+  name: 'Carpool App',
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://carpooling-app-271518.firebaseio.com'
+});
+
+var registrationToken = 'cuY66e1BRSewMBDpncXfmz:APA91bGY3o9K7IAOWtiElHysVHj690j9QlOwenSjswfC_FsaE886dYNAu7TvxkKLdT59WNJ6_xrMzYeLdObX-D5z_s11JXg7PH4IRu3ocfurQYVSJ2PzKUvhZYpcZpraGY3-HtXzfjeh';
 
 var message = {
-  data: {
-    score: '850',
-    time: '2:45'
+  notification: {
+    title: 'Carpool App',
+    body: ' this is my message'
   },
+  android: {
+    ttl: 3600 * 1000,
+    notification: {
+      icon: 'stock_ticker_update',
+      color: '#f45342',
+    },
+  },
+
   token: registrationToken
 };
 
