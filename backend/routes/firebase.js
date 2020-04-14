e3107bd5e5623a1beb49545904e09dd2ab93551e
Evan de Jesus <evan.dejesus@24g.com>
2020-04-14 15:35:54 -0400

    fix firebase

--- backend/routes/firebase.js
+++ backend/routes/firebase.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const respond = require('../util/respond');
+const validate = require('../util/validate');
 
 const router = express.Router();
 const controller = require('../controllers/firebase');
 
-router.post('/', controller.post);
+router.post('/', async (req, res) => {
+  const b = req.body;
+  if (!validate(b, {
+    users: 'array',
+    message: 'string'
+  }, res)) return;
+
+  try {
+    const notify = await controller.post(req, res);
+    respond(200, notify, res);
+  }
+  catch (err) {
+    respond(500, err, res);
+  }
+});
 
 module.exports = router;
